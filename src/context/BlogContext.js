2ff3ff3229419a74e93c2b61d21046e8c1544ab3
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-19 17:47:22 +0200

    added dynamic data source with useState

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //take it as a pipe  , it will move the information from the Provider , to the Blog 
 const BlogContext = React.createContext();
@@ -6,8 +6,21 @@ const BlogContext = React.createContext();
 // export : the Blogcontext obj will be used inside App.js file , not Default because the default is BlogContext 
 export const BlogProvider = ({ children }) => {
 
-    const BlogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }, { title: 'Blog Post #3' }, { title: 'Blog Post #4' },]
-    return <BlogContext.Provider value={BlogPosts}>
+    //STATIC DATA SOURCE 
+    // const BlogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }, { title: 'Blog Post #3' }, { title: 'Blog Post #4' },]
+    // DYNAMIC DATA SOURCE 
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    const addBlogPost = () => {
+        //setter to pass the new value
+        setBlogPosts([
+            ...blogPosts, {
+                title: `Blog Post #${blogPosts.length + 1}`
+            }
+        ]);
+    }
+
+    return <BlogContext.Provider value={{ data : blogPosts , addBlogPost}}>
         {children}
     </BlogContext.Provider>
 };
