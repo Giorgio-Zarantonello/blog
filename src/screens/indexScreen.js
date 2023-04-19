5a8e17d17dff872fee29c1b15a5943c4160e8c17
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-19 15:33:20 +0200

    new file:   src/screens/indexScreen.js

---
+++ src/screens/indexScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { View , Text , StyleSheet } from "react-native";
+
+const indexScreen = () => {
+    return (
+        <View>
+            <Text>indexScreen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default indexScreen;
