dc268940ec85061e4e374918cfc49527ecfcbd46
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-19 22:01:22 +0200

    .

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 import { FlatList } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import { version } from "react/cjs/react.production.min";
+
 
 
 const indexScreen = () => {
@@ -15,14 +18,35 @@ const indexScreen = () => {
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>
-                }
-                }
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <AntDesign style={styles.icon} name="delete" />
+                        </View>
+                    );
+                }}
             />
         </View>
-    )
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+
+    },
+    title: {
+        fontSize: 18,
+
+    },
+    icon: {
+        fontSize: 25,
+    }
+});
 
 export default indexScreen;
