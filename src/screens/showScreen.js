5ac0273f3224933c9cfd9d8c484b915afa285eaf
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-20 13:34:38 +0200

    .

--- src/screens/showScreen.js
+++ src/screens/showScreen.js
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Context } from '../context/BlogContext';
+import { Context as BlogContext } from '../context/BlogContext';
 
 const ShowScreen = ({ navigation }) => {
-    const { state } = useContext(Context)
-    const blogPost = state.find( blogPost => blogPost.id === navigation.getParam('id'));
+    const { state } = useContext(BlogContext)
+    const blogPost = state.find( 
+        blogPost => blogPost.id === navigation.getParam('id'));
     
     return (
         <View>
